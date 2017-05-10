fc9e7a447b18e46496399bed62e2fe7b206ae066
Mathieu Rodic <mathieu@rodic.fr>
2017-05-10 14:40:47 +0200

    Added a setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pyActiveSync',
+    version = '0.2',
+    description = 'pyActiveSync is a pure python client-side library for the Microsoft ActiveSync protocol.',
+    author = 'Mathieu Rodic',
+    author_email = 'mathieu@iko-system.com',
+    packages = find_packages(),
+    license = 'GNU GENERAL PUBLIC LICENSE',
+    install_requires = [],
+    zip_safe = True,
+)
